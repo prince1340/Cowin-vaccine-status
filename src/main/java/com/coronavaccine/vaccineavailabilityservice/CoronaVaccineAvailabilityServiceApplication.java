8b9099dbb62d65ebf9ba5f6b2b64be776aa73fc2
prince1340 <aec.prince@gmail.com>
2021-05-23 13:52:14 +0530

    Initial project setup.

---
+++ src/main/java/com/coronavaccine/vaccineavailabilityservice/CoronaVaccineAvailabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coronavaccine.vaccineavailabilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVaccineAvailabilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVaccineAvailabilityServiceApplication.class, args);
+	}
+
+}
